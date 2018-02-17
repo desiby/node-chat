b205610e04ba2b2e634f4fb2e3919bf7f9819b3a
Desire <nbahbio@wgu.edu>
2018-02-17 00:30:22 -0500

    handle connect and disconnect events

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const app = require("express")();
 const http = require("http").Server(app);
+const io = require("socket.io")(http);
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
+
+io.on("connection", (socket) => {
+    //send the "connected" event to all users
+    io.emit("connected");
+  //send msg to all users  
+  socket.on("chat message", (msg) => {
+     io.emit("chat message", msg);
+  }); 
+  //notify all users whenever a user disconnects 
+  socket.on("disconnect", () => {
+    io.emit("disconnect");
+ });
+ 
+});
+
 
 http.listen(3000, () => {
     console.log("listening on port 3000...")
